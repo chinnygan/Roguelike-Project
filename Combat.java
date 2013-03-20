41b74b1d0efbb105a3a59c154ab00556f78ef8e1
Marton <mlt.residence@gmail.com>
2013-03-20 22:14:57 +0000

    Update Combat.java

--- Combat.java
+++ Combat.java
@@ -27,7 +27,33 @@ public class Combat {
 		
 		int mLevel = Monster.getLevel(m);
 		return Player.getXP(p) + calculateXP(p, mLevel);
+		//or: Player.giveXP(calculateXP(p, mLevel));
 		
 	}
 	
 	
+	
+	public Combat(Agent attacker, Agent defender){
+		
+		int attack = getAttack(attacker);
+		int defence = getDefence(defender);
+		int newHP;
+		
+		if (attack<=defence){
+			newHP = 0;
+		} else {
+			newHP = Defender.getHP()-(defence-attack);
+		}
+		
+		Player.setHP(newHP); 
+		//This is supposing the attacker is a monster.
+		//I don't really get the concept behind this method.
+
+	}
+
+	public void heal(int n){
+		
+		Player.setHp(Player.getHp()+n);
+		
+	}
+}
