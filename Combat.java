2eae3e408b41cadd215e2d858022e7c533906746
Rosswest <rosswest@hotmail.co.uk>
2013-03-28 18:10:33 +0000

    Update Combat.java
    Update from 2 days ago, use mediafire

--- Combat.java
+++ Combat.java
@@ -1,59 +1,73 @@
-
 public class Combat {
-	
-	
-	
-	
-	public int calculateXP(Player p, int mLevel){
+	  
+
+	private static double hp; //player's health
+	private static double lvl;   //which level the player's on
+
+	private static double damage;
+	//the amount of damage the player/hostile can cause
+
+	private int itemValue; 
+	//can be a multiplier for damage, imported from Item text file
+
+	public static void calculateXP(Player p, int mLevel){
 		int base = 300;
-		int pLevel = Player.getLevel(p);
+		int pLevel = p.getLevel();
 		int newXP = 0;
-		
+
 		if ( pLevel<=3 && mLevel<=3 && pLevel != mLevel){
 			newXP = pLevel*base;
-			
+
 		}else if ( pLevel == mLevel) {
 			newXP = pLevel*base;
-			
+
 		}else {
 			newXP = (mLevel-pLevel)*base/2 + pLevel*base; 
 		}
-		
-		return newXP;
-		
-	}
-	
-	public int giveXP(Player p, Monster m){
-		
-		int mLevel = Monster.getLevel(m);
-		return Player.getXP(p) + calculateXP(p, mLevel);
-		//or: Player.giveXP(calculateXP(p, mLevel));
-		
-	}
-	
-	
-	
-	public Combat(Agent attacker, Agent defender){
-		
-		int attack = getAttack(attacker);
-		int defence = getDefence(defender);
-		int newHP;
-		
-		if (attack<=defence){
-			newHP = 0;
-		} else {
-			newHP = Defender.getHP()-(defence-attack);
-		}
-		
-		Player.setHP(newHP); 
-		//This is supposing the attacker is a monster.
-		//I don't really get the concept behind this method.
+
+		giveXP(p, newXP);
 
 	}
 
-	public void heal(int n){
-		
-		Player.setHp(Player.getHp()+n);
+	public static void giveXP(Player p, int xp){
+
+		p.setXP(p.getXP() + xp);
+
+	}
+	
+	public static double damage(){
+		double multiplier = (double)(Math.random() * (lvl/10));
+		damage = multiplier * 100;
+		return damage;
+	}
+
+	public static double returnHp(){
+	//calculates new hp of player, given current health (double hp) and current level (int lvl) in the game	
+
+		hp = hp  - damage(); //sets player's health to new value after fight
+		return hp;		   //also returns player's health (double hp)
+
+	}
+
+	//test
+	public static void main(String[] args) {
+		Player p = new Player(0,0);
+		int monster = 3;
+		calculateXP(p,5);
+	}
+
+	public static void heal(int pow, Player p) {
+		p.setHp(p.getHp() + pow);
 		
 	}
+
+
+
 }
+
+
+
+
+
+	
+
